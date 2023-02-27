d4ac6b007c2e99e50d6d924e248bcb07485ff554
Rafael Ottersberg <rafael.ottersberg@gmx.ch>
2023-02-27 09:57:37 +0100

    implemented body struct

--- Main.jl
+++ Main.jl
@@ -1,5 +1,6 @@
 using BenchmarkTools
 include("NBodySolver.jl")
-x, y, z, vx, vy, vz, m = readdata("solar_jfc.dat")
-leapfrog!(x, y, z, vx, vy, vz, m, 6.67408e-11, 24*60*60, 100)
-print(@benchmark leapfrog!(x, y, z, vx, vy, vz, m, 6.67408e-11, 24*60*60, 100))
+bodies = readdata("solar_jfc.dat")
+leapfrog!(bodies, 6.67408e-11, 24*60*60, 100)
+trial = @benchmark leapfrog!(bodies, 6.67408e-11, 24*60*60, 100)
+print(mean(trial))
